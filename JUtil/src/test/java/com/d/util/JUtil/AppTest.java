706eca245682432d27e60b53baf3a50712ec29ce
Jester6 <Tomek@DESKTOP-S9J9BKP>
2024-12-02 18:54:24 +0100

    update

--- JUtil/src/test/java/com/d/util/JUtil/AppTest.java
+++ JUtil/src/test/java/com/d/util/JUtil/AppTest.java
@@ -2,18 +2,17 @@ package com.d.util.JUtil;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
-    }
+	@ParameterizedTest
+	@CsvSource({
+			"13.4, 22.7",
+			"2.9, -7.4"
+	})
+	public void shouldAnswerWithTrue(double r, double i) {
+		System.out.println(new Complex(r, i).root(2.72));
+		assertTrue(true);
+	}
 }
