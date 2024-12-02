bde614f3685639b99b809a224212242948042433
Tomek <Tomek@DESKTOP-S9J9BKP>
2024-12-02 15:26:36 +0100

    added Complex class

---
+++ JUtil/src/main/java/com/d/util/JUtil/Complex.java
@@ -0,0 +1,17 @@
+package com.d.util.JUtil;
+
+public class Complex {
+
+	private double realPart;
+	private double imaginaryPart;
+
+	public Complex() { this(0, 0); }
+
+	public Complex(double imaginary) { this(0, imaginary); }
+
+	public Complex(double real, double imaginary) {
+		this.realPart = real;
+		this.imaginaryPart = imaginary;
+	}
+
+}
